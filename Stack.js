962141200ab0f9221e49d6294a57aa0b92cc890f
justaskulkarni <98650743+justaskulkarni@users.noreply.github.com>
2022-12-11 12:23:42 +0530

    Update Stack.js

--- Stack.js
+++ Stack.js
@@ -1,12 +1,41 @@
 const pushbtn = document.getElementById('push');
-const pushinput = document.getElementById('pushinput');
+const popbtn = document.getElementById('pop');
+const peekbtn = document.getElementById('peek');
+const sizebtn = document.getElementById('size');
+const isEmptybtn = document.getElementById('isEmpty');
 const submitElementBtn = document.getElementById('submitElement');
 const element = document.getElementsByClassName('element');
 const box  =document.getElementsByClassName('box');
+
+
 pushbtn.addEventListener('click', function onClick(event) {
     pushinput.style.display = "block";
 });
+
+popbtn.addEventListener('click', function onClick(event){
+    const popoutput = document.getElementById('popoutput');
+    popoutput.style.display = "block";    
+});
+
+peekbtn.addEventListener('click', function onClick(event){
+    const peekoutput = document.getElementById('peekoutput');
+    peekoutput.style.display = "block";    
+});
+
+sizebtn.addEventListener('click', function onClick(event){
+    const sizeoutput = document.getElementById('sizeoutput');
+    sizeoutput.style.display = "block";    
+});
+
+isEmptybtn.addEventListener('click', function onClick(event){
+    const isEmptyoutput = document.getElementById('isEmptyoutput');
+    isEmptyoutput.style.display = "block";    
+});
+
 submitElementBtn.addEventListener('click', function(){
-    const targetElement = document.getElementById('pushinput');
+    const targetElement = document.getElementById('pushoutput');
+    const enteredElement = document.getElementById('e').value;
+    targetElement.innerHTML = enteredElement;
+    targetElement.style.display = "block";
     targetElement.className = "box";
 });
